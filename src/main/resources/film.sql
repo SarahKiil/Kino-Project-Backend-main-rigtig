cd8aad65347873ac426c1b232d0ef4c4e3b051d9
asger <asgervb@gmail.com>
2024-10-09 11:59:33 +0200

    har lavet en film.sql

---
+++ src/main/resources/film.sql
@@ -0,0 +1,60 @@
+INSERT INTO film (
+    title, year, rated, released, runtime, genre, director, writer, actors,
+    plot, plotDK, poster, metascore, imdb_rating, imdb_votes, imdbid, website, response, is_ongoing
+)
+VALUES
+    (
+        'Inception', '2010', 'PG-13', '2010-07-16', '148 min',
+        'Action, Sci-Fi, Thriller', 'Christopher Nolan', 'Christopher Nolan',
+        'Leonardo DiCaprio, Joseph Gordon-Levitt, Ellen Page',
+        'A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O.',
+        'En tyv, der stjæler forretningshemmeligheder gennem brugen af ​​drømme-delingsteknologi, får den omvendte opgave at plante en idé i sindet på en direktør.',
+        'https://m.media-amazon.com/images/M/MV5BMTM2NTI5OTQ3OV5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg',
+        '74', '8.8', '2,000,000', 'tt1375666',
+        'https://www.imdb.com/title/tt1375666/', 'True', false
+    );
+INSERT INTO film (
+    title, year, rated, released, runtime, genre, director, writer, actors,
+    plot, plotDK, poster, metascore, imdb_rating, imdb_votes, imdbid, website, response, is_ongoing
+)
+VALUES
+    (
+        'The Matrix', '1999', 'R', '1999-03-31', '136 min',
+        'Action, Sci-Fi', 'Lana Wachowski, Lilly Wachowski', 'Lilly Wachowski, Lana Wachowski',
+        'Keanu Reeves, Laurence Fishburne, Carrie-Anne Moss',
+        'A computer hacker learns from mysterious rebels about the true nature of his reality and his role in the war against its controllers.',
+        'En computerhacker lærer af mystiske oprørere om den sande natur af hans virkelighed og hans rolle i krigen mod dets kontrollører.',
+        'https://m.media-amazon.com/images/M/MV5BNzQzOTk3NzAtNjYwMC00ZDI4LWE0ZTUtOTNlZGFmNjdmZWRlXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg',
+        '73', '8.7', '1,700,000', 'tt0133093',
+        'https://www.imdb.com/title/tt0133093/', 'True', false
+    );
+INSERT INTO film (
+    title, year, rated, released, runtime, genre, director, writer, actors,
+    plot, plotDK, poster, metascore, imdb_rating, imdb_votes, imdbid, website, response, is_ongoing
+)
+VALUES
+    (
+        'The Dark Knight', '2008', 'PG-13', '2008-07-18', '152 min',
+        'Action, Crime, Drama', 'Christopher Nolan', 'Jonathan Nolan, Christopher Nolan',
+        'Christian Bale, Heath Ledger, Aaron Eckhart',
+        'When the menace known as the Joker emerges from his mysterious past, he wreaks havoc and chaos on the people of Gotham.',
+        'Når truslen kendt som Jokeren dukker op fra sin mystiske fortid, forårsager han kaos og uorden blandt befolkningen i Gotham.',
+        'https://m.media-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_SX300.jpg',
+        '84', '9.0', '2,400,000', 'tt0468569',
+        'https://www.imdb.com/title/tt0468569/', 'True', false
+    );
+INSERT INTO film (
+    title, year, rated, released, runtime, genre, director, writer, actors,
+    plot, plotDK, poster, metascore, imdb_rating, imdb_votes, imdbid, website, response, is_ongoing
+)
+VALUES
+    (
+        'The Godfather', '1972', 'R', '1972-03-24', '175 min',
+        'Crime, Drama', 'Francis Ford Coppola', 'Mario Puzo, Francis Ford Coppola',
+        'Marlon Brando, Al Pacino, James Caan',
+        'The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.',
+        'Den aldrende patriark af en organiseret forbrydelsesdynasti overdrager kontrollen med sit hemmelige imperium til sin modvillige søn.',
+        'https://m.media-amazon.com/images/M/MV5BM2MyNjYxNmYtYTAwMC00ZjQ5LWEzNjktYTc0ZGMzNWE2YzBiXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg',
+        '100', '9.2', '1,700,000', 'tt0068646',
+        'https://www.imdb.com/title/tt0068646/', 'True', false
+    );
